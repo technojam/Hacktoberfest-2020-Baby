11538c2d54a903a3086aa2567b6b5530f2b73a10
Urfa Ansari <55250956+Urfafazli@users.noreply.github.com>
2020-10-02 21:29:32 +0530

    Create luhn.py

---
+++ luhn.py
@@ -0,0 +1,35 @@
+#to create a function that validates a credit card number
+def luhn(card_number):
+    def digits_of(n):
+        return [int(d) for d in str(n)]
+    digits = digits_of(card_number)
+    print(digits)
+    odd_digits = digits[-1::-2]
+    print(odd_digits)
+    even_digits = digits[-2::-2]
+    print(even_digits)
+    checksum = 0
+    checksum += sum(odd_digits)
+    print(checksum)
+    print("_________________________________________________________________________________")
+    for d in even_digits:
+        checksum += sum(digits_of(d*2))
+    print(d)
+    print("_________________________________________________________________________________")
+    print(digits_of(d))
+    print("_________________________________________________________________________________")
+    print(digits_of(d * 2))
+    print("_________________________________________________________________________________")
+    print(sum(digits_of(d)))
+    print("_________________________________________________________________________________")
+    print(sum(digits_of(d*2)))
+    print("_________________________________________________________________________________")
+    print(checksum)
+    print(checksum%10)
+    return (checksum % 10)
+
+#A little code to test it out on a number
+if (luhn("8830723086640451")):
+    print("INVALID")
+else:
+    print("VALID")
