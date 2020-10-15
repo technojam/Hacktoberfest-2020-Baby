988d162112c7b880b9b694e19c2eddbfe4886845
GulameAshraf <70468921+GulameAshraf@users.noreply.github.com>
2020-10-16 00:19:13 +0530

    Area.c

---
+++ tsa_of_hollow_cylinder.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int r1,r2,h;
+    float tsa;
+    printf("enter the r1 of hollow cylinder :",r1);
+    scanf("%d",&r1);
+    printf("enter the r2 of hollow cylinder :",r2);
+    scanf("%d",&r2);
+    printf("enter the height of hollow cylinder :",h)
+    scanf("%d",&h);
+    tsa = 6.282 * (r1 + r2) * (h + r1 +r2);
+    printf("Total surface area of hollow cylinder --->%f",tsa);
+    return 0;
+}
