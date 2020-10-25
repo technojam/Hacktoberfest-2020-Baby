329b12307afe45650c2f0bfc58d028bb61ff8221
ritik1123 <67791597+ritik1123@users.noreply.github.com>
2020-10-25 08:27:29 +0530

    create area of circle and volume of cylinder

---
+++ area_and_volume.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int r;
+    float a=3.14;
+    printf("the radius of a circle is ");
+    scanf("%d",&r);
+int area;
+  //  printf("the area of circle is %f",a*r*r);
+    //scanf("%f",&area);
+    int h;
+    printf("the height of cylinder is ");
+    scanf("%d",&h);
+    printf("the volume of cylinder is %f",a*r*r*h);
+    scanf("%f",&area);
+    return 0;
+}
