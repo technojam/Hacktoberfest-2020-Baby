640d8ef5ecfe2e1dd08134546df0df184666bedd
TechSajal <57318794+TechSajal@users.noreply.github.com>
2020-10-30 20:32:40 +0530

    Create GetMyIPAddress.java

---
+++ GetMyIPAddress.java
@@ -0,0 +1,11 @@
+import java.net.InetAddress;
+class GetMyIPAddress
+{
+   public static void main(String args[]) throws Exception
+   {
+      
+      InetAddress myIP=InetAddress.getLocalHost();
+      System.out.println("My IP Address is:");
+      System.out.println(myIP.getHostAddress());
+  }
+}
