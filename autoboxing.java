bc7117ac4cdd0582b65c6a9374d06cb72b9483e3
rashmisohi100 <73738888+rashmisohi100@users.noreply.github.com>
2020-10-31 14:59:17 +0530

    created autoboxing.java
    to show AutoBoxing and Unboxing

---
+++ autoboxing.java
@@ -0,0 +1,15 @@
+import java.io.*; 
+class GFG 
+{ 
+	public static void main (String[] args) 
+	{  
+		Integer i = new Integer(10);  
+		int i1 = i; 
+                       System.out.println("Value of i: " + i); 
+		System.out.println("Value of i1: " + i1); 
+		Character gfg = 'a';  
+		char ch = gfg; 
+		System.out.println("Value of ch: " + ch); 
+		System.out.println("Value of gfg: " + gfg); 
+            } 
+}
