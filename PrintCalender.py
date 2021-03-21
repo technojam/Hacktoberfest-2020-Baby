50eac4e9cf66a7d40c80ff241395eeabacb56d0b
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-03-22 04:07:42 +0530

    this is the way

--- PrintCalender.py
+++
@@ -1,5 +0,0 @@
-import calendar
-#ASK FOR ENTER MONTH AND YEAR
-year=int(input("Enter the year : "))
-month=int(input("Enter the month: "))
-print(calendar.month(year,month))
--- dictionary.py
+++
@@ -1,7 +0,0 @@
-Names= { 152;"name_A",
-         254;"name_B",
-         524;"name_c",
-         754;"name_D"
-}
-
-print(Names.get(___,"this name don't exist")) 
